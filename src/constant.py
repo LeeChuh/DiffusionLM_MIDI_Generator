20623e30d1ba91ae96d16d8d736fe66b4db2eb25
Chuhan Li <lich@bu.edu>
2023-07-13 14:46:07 +0800

    Create constant.py

---
+++ src/constant.py
@@ -0,0 +1,3 @@
+DATA_PATH = "/Users/chuhanli/Desktop/adl-piano-midi-master/midi/adl-piano-midi/Ambient/Ambient/Roger Eno/Sunburst (Album Version).mid"
+MIN_NOTE_MULTIPLIER = 0.125
+MIDI_EXTENSIONS = [".mid", ".midi"]
